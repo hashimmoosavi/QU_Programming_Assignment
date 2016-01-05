0ef537550959c2778f033e371747a59872f386f3
hashimmoosavi <hashimmoosavi@Hashims-MacBook-Pro.local>
2016-01-05 22:35:04 +0300

    Sending 2 APKs in threads as a post requests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QU_Programming_Assignment
+
+Android App that sends application apk's to a web server using POST requests
--- app/src/main/java/com/example/hashimmoosavi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hashimmoosavi/myapplication/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.hashimmoosavi.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    int serverResponseCode = 0;
+    TextView maintext = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,36 +55,142 @@ public class MainActivity extends AppCompatActivity {
     private void getFiles()
     {
         // location where apps are stored
-        String systemAppsDir = "/system/app";
-        String userAppsDir = "/data/app";
-        String sdCardAppsDir = "/mnt/asec";
+        final String systemAppsDir = "/system/app";
+        final String userAppsDir = "/data/app";
+        final String sdCardAppsDir = "/mnt/asec";
 
-        File appsDir = new File(sdCardAppsDir);
+        File appsDir = new File(systemAppsDir);
 
-        TextView maintext = (TextView) findViewById(R.id.maintext);
-        maintext.setText("Following are the apks:\n");
+        maintext = (TextView) findViewById(R.id.maintext);
 
         if(appsDir != null) {
-            String files[] = appsDir.list();
+            final String files[] = appsDir.list();
 
-            for (int i = 0; i < files.length; i++) {
-                File currentAppDir = new File(sdCardAppsDir + "/" + files[i] + "/");
+            for (int i = 0; i < files.length - (files.length - 2); i++) {   // UPLOADING ONLY 2 APKS FOR EXAMPLE
+                File currentAppDir = new File(systemAppsDir + "/" + files[i] + "/");
 
                 if (currentAppDir != null) {
-                    String[] apks = currentAppDir.list();
+                    final String[] apks = currentAppDir.list();
 
                     for (int j = 0; j < apks.length; j++) {
 
                         // Find the apks
 
                         if (apks[j].lastIndexOf(".apk") > -1) {
-                            maintext.append(apks[j]);
-                            maintext.append("\n");
+
+                            // Send post request with the found apk file in new network thread
+
+                            final int indexi = i;
+                            final int indexj = j;
+
+                            Thread t = new Thread(new Runnable() {
+                                public void run() {
+                                    runOnUiThread(new Runnable() {
+                                        public void run() {
+                                            maintext.setText("Uploading started.....\n");
+                                        }
+                                    });
+                                    int response= uploadFile(systemAppsDir + "/" + files[indexi] + "/" + apks[indexj]);
+                                    System.out.println("RES : " + response);
+                                    maintext.append("Uploaded " + apks[indexj] + "\n");
+                                }
+                            });
+
+                            t.start();
+
+                            while(t.isAlive())
+                            {
+                                // Waiting for thread to get over
+                            }
                         }
                     }
                 }
             }
         }
+    }
 
+    public int uploadFile(String sourceFileUri) {
+        String upLoadServerUri = "http://192.168.100.7:8000";
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+        if (!sourceFile.isFile()) {
+            Log.e("uploadFile", "Source File Does not exist");
+            return 0;
+        }
+        try { // open a URL connection to the Servlet
+            FileInputStream fileInputStream = new FileInputStream(sourceFile);
+            URL url = new URL(upLoadServerUri);
+            conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("uploaded_file", fileName);
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""+ fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
+
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            serverResponseCode = conn.getResponseCode();
+            String serverResponseMessage = conn.getResponseMessage();
+
+            Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
+            if(serverResponseCode == 200){
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        maintext.setText("File Upload Completed.");
+                        Toast.makeText(MainActivity.this, "File Upload Complete.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            //close the streams
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+            Toast.makeText(MainActivity.this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+            Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Exception : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("Upload file to server", "Exception : " + e.getMessage(), e);
+        }
+        return serverResponseCode;
     }
 }
